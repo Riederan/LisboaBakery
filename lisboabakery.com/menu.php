eeba5ac16636eb2e50d13aca1ba56f6080f6618d
SynWade <nebojsa.orescanin@gmail.com>
2017-01-13 19:57:45 -0500

    The menu is partially complete, passing it to raul

--- lisboabakery.com/menu.php
+++ lisboabakery.com/menu.php
@@ -14,6 +14,571 @@
 			<div class="page-header">
 				<h1>Menu <small>Best Portuguese Chicken in Town!</header></h1>
 			</div>
+			<!--Front side menu-->
+			<div>
+				<h1>Charcoal BBQ Grilled Portuguese Chicken/Frango no Churrasco</h1>
+				<p>Whole: $12.49</p>
+				<p>Whole: $9.49</p>
+				<p>Whole: $13.49</p>
+				<p>Whole: $11.49</p>
+				<p>Whole: $10.49</p>
+				<p>*Extra sauce $0.75/ea.</p>
+				<p>*355ml Bottle of sauce $3.99/ea.</p>
+			</div>
+			<div>
+				<h1>Side Orders/Ordem Laterais</h1>
+				<table>
+					<tr>
+						<td>
+						</td>
+						<td>
+							Small
+						</td>
+						<td>
+							Medium
+						</td>
+						<td>
+							Large
+						</td>
+					</tr>
+					<tr>
+						<td>
+						</td>
+						<td>
+							(1-2)
+						</td>
+						<td>
+							(3-4)
+						</td>
+						<td>
+							(4-6)
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Roasted Potatoes
+						</td>
+						<td>
+							$6.99
+						</td>
+						<td>
+							$8.99
+						</td>
+						<td>
+							$10.49
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Vegetables
+						</td>
+						<td>
+							$6.99
+						</td>
+						<td>
+							$8.99
+						</td>
+						<td>
+							$10.49
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Rice
+						</td>
+						<td>
+							$5.99
+						</td>
+						<td>
+							$7.99
+						</td>
+						<td>
+							$9.49
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Fries/Onion Rings
+						</td>
+						<td>
+							$5.99
+						</td>
+						<td>
+							$7.99
+						</td>
+						<td>
+							$9.49
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<h3>Salads/Salada</h3>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							House
+						</td>
+						<td>
+							$4.99
+						</td>
+						<td>
+							N/A
+						</td>
+						<td>
+							$8.99
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Caesar
+						</td>
+						<td>
+							$5.49
+						</td>
+						<td>
+							N/A
+						</td>
+						<td>
+							$9.99
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Greek
+						</td>
+						<td>
+							$5.49
+						</td>
+						<td>
+							N/A
+						</td>
+						<td>
+							$9.99
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Chick Pea
+						</td>
+						<td>
+							$4.99
+						</td>
+						<td>
+							N/A
+						</td>
+						<td>
+							$8.99
+						</td>
+					</tr>
+				</table>
+				<p><b>*Check out our Salad Bar for more salad options</b></p>
+				<p>*Add Chicken Breast to any salad for $3.99/ea.</p>
+			</div>
+			<!--Back side menu-->
+			<div>
+				<h1>Catering</h1>
+				<p> Leit&atilde;o (Suckling Pig) Whole $150.00 Half $90.00</p>
+				<p> Roasted Rabbit $14.99Lb</p>
+				<p> Roasted Lamb $14.99Lb</p>
+				<p>*Requires 72 hr. notice</p>
+			</div>
+			<div>
+				<h1>Trays/Tabuleiros</h1>
+				<table>
+					<tr>
+						<td>
+						</td>
+						<td>
+							Small
+						</td>
+						<td>
+							Medium
+						</td>
+						<td>
+							Large
+						</td>
+						<td>
+							X-Large
+						</td>
+					</tr>
+					<tr>
+						<td>
+						</td>
+						<td>
+							(6-8)
+						</td>
+						<td>
+							(9-12)
+						</td>
+						<td>
+							(13-23)
+						</td>
+						<td>
+							(24-50)
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Roasted Potatoes/Batata
+						</td>
+						<td>
+							$19.99
+						</td>
+						<td>
+							$25.99
+						</td>
+						<td>
+							$35.99
+						</td>
+						<td>
+							$49.99
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Rice/Arroz
+						</td>
+						<td>
+							$17.99
+						</td>
+						<td>
+							$23.99
+						</td>
+						<td>
+							$33.99
+						</td>
+						<td>
+							$46.99
+						</td>
+					</tr>
+					<tr>
+						<td>
+							*Seafood Rice/ Arroz de Marisco
+						</td>
+						<td>
+							N/A
+						</td>
+						<td>
+							$75.00
+						</td>
+						<td>
+							$110.00
+						</td>
+						<td>
+							$160.00
+						</td>
+					</tr>
+					<tr>
+						<td>
+							*Octopus Rice/Arroz Polvo
+						</td>
+						<td>
+							N/A
+						</td>
+						<td>
+							$75.00
+						</td>
+						<td>
+							$110.00
+						</td>
+						<td>
+							$160.00
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Vegetable/Vegeta&iacute;s
+						</td>
+						<td>
+							$19.99
+						</td>
+						<td>
+							$25.99
+						</td>
+						<td>
+							$35.99
+						</td>
+						<td>
+							$49.99
+						</td>
+					</tr>
+					<tr>
+						<td>
+							French Fries/Batatas Fritas
+						</td>
+						<td>
+							$16.99
+						</td>
+						<td>
+							$21.99
+						</td>
+						<td>
+							$32.99
+						</td>
+						<td>
+							$39.99
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Onion Rings/An&eacute;&iacute;s de Cebola
+						</td>
+						<td>
+							$16.99
+						</td>
+						<td>
+							$21.99
+						</td>
+						<td>
+							$32.99
+						</td>
+						<td>
+							$39.99
+						</td>
+					</tr>
+					<tr>
+						<td>
+							House Salad/Salada
+						</td>
+						<td>
+							$16.99
+						</td>
+						<td>
+							$21.99
+						</td>
+						<td>
+							$29.99
+						</td>
+						<td>
+							$38.99
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Caesar Salad/Salada C&eacute;sar
+						</td>
+						<td>
+							$18.99
+						</td>
+						<td>
+							$25.99
+						</td>
+						<td>
+							$35.99
+						</td>
+						<td>
+							$45.99
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Greek Salad/Salada Grega
+						</td>
+						<td>
+							$23.99
+						</td>
+						<td>
+							$29.99
+						</td>
+						<td>
+							$38.99
+						</td>
+						<td>
+							$48.99
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Bacalhau &agrave; Bras
+						</td>
+						<td>
+							$44.99
+						</td>
+						<td>
+							$69.99
+						</td>
+						<td>
+							$99.99
+						</td>
+						<td>
+							$149.99
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Bacalhau com Natas
+						</td>
+						<td>
+							$44.99
+						</td>
+						<td>
+							$69.99
+						</td>
+						<td>
+							$99.99
+						</td>
+						<td>
+							$149.99
+						</td>
+					</tr>
+					<tr>
+						<td>
+							*Meat/Carne Lasagna
+						</td>
+						<td>
+							$32.99
+						</td>
+						<td>
+							$44.99
+						</td>
+						<td>
+							$59.99
+						</td>
+						<td>
+							N/A
+						</td>
+					</tr>
+					<tr>
+						<td>
+							*Vegetable/Vegeta&iacute;s Lasagna
+						</td>
+						<td>
+							$37.99
+						</td>
+						<td>
+							$49.99
+						</td>
+						<td>
+							$64.99
+						</td>
+						<td>
+							N/A
+						</td>
+					</tr>
+					<tr>
+						<td>
+							*Saut&eacute;ed Mussels and Shrimp
+						</td>
+						<td>
+							N/A
+						</td>
+						<td>
+							$80.00
+						</td>
+						<td>
+							$110.00
+						</td>
+						<td>
+							$150.00
+						</td>
+					</tr>
+					<tr>
+						<td>
+							*Octopus Stew/Polvo Guisado
+						</td>
+						<td>
+							$49.99
+						</td>
+						<td>
+							$74.99
+						</td>
+						<td>
+							$104.99
+						</td>
+						<td>
+							$154.99
+						</td>
+					</tr>
+					<tr>
+						<td>
+							*Meat & Bean Stew/Fe&iacute;joada
+						</td>
+						<td>
+							$34.99
+						</td>
+						<td>
+							$44.99
+						</td>
+						<td>
+							$54.99
+						</td>
+						<td>
+							$69.99
+						</td>
+					</tr>
+					<tr>
+						<td>
+							*Meat Stew/Came Gu&iacute;sado
+						</td>
+						<td>
+							$60.00
+						</td>
+						<td>
+							$80.00
+						</td>
+						<td>
+							$110.00
+						</td>
+						<td>
+							$150.00
+						</td>
+					</tr>
+					<tr>
+						<td>
+							*Portuguese Pork and Clams<br>/Carne de Porco Alentenjana
+						</td>
+						<td>
+							N/A
+						</td>
+						<td>
+							$70.00
+						</td>
+						<td>
+							$100.00
+						</td>
+						<td>
+							$140.00
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<b>Coffee</b>
+						</td>
+						<td>
+						</td>
+						<td>
+						</td>
+						<td>
+						</td>
+						<td>
+							$100.00
+						</td>
+					</tr>
+				</table>
+				<p>Includes 120 cups, stir sticks, sugar, cream, and milk</p>
+				<p style="text-align:center"><b>*Requires 24-48 hours notice.</b></p>
+				<p style="text-align:center"><b>*Taxes not included. Subject to availability. Prices are subject to change without prior notice.</b></p>
+			</div>
+			<div>
+				<h1>Each Piece/Cada Pe&ccedil;a</h1>
+				<p>Sea Bass Fillet/Filet de Peixe $3.25</p>
+				<p>*Roast Beef with Beef Gravy $3.00</p>
+				<p>Pork Cutlets/Febras de Porco $5.00</p>
+				<p>Sautéed onions and peppers/Cebolada $1.00</p>
+				<p>Sea Bass Fillet/Filet de Peixe $3.25</p>
+				<p>*Roast Beef with Beef Gravy $3.00</p>
+				<p>Sea Bass Fillet/Filet de Peixe $3.25</p>
+				<p>*Roast Beef with Beef Gravy $3.00</p>
+				<p>Sea Bass Fillet/Filet de Peixe $3.25</p>
+				<p>*Roast Beef with Beef Gravy $3.00</p>
+				<p>Sea Bass Fillet/Filet de Peixe $3.25</p>
+				<p>*Roast Beef with Beef Gravy $3.00</p>
+				<p>Sea Bass Fillet/Filet de Peixe $3.25</p>
+				<p>*Roast Beef with Beef Gravy $3.00</p>
+				<p>Sea Bass Fillet/Filet de Peixe $3.25</p>
+				<p>*Roast Beef with Beef Gravy $3.00</p>
+			</div>
 		<?php include('footer.php'); ?>
 	</body>
 </html>
