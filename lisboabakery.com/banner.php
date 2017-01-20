71117cc60797aac67aa55d6949f1e0d91216886d
Raul Iederan <Raul Iederan>
2017-01-20 07:26:17 -0500

    updated UI for menu only.

--- lisboabakery.com/banner.php
+++ lisboabakery.com/banner.php
@@ -1,33 +1,38 @@
 <header>
 	<img class="img-responsive m-x-auto d-block" src="lisboaLogo.png" alt="Lisboa B&G Logo" /></br>
-		<nav class="navbar navbar-inverse">
+		<nav class="navbar navbar-inverse" role="navigation">
 			<div class="container-fluid">
 			<!-- Brand and toggle get grouped for better mobile display -->
 				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-brand-centered">
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="#">Lisboa Bakery & Grill</a>
+					<a class="navbar-brand navbar-brand-centered" href="#">Lisboa Bakery & Grill</a>
 				</div>
 			<!-- Collect the nav links, forms, and other content for toggling -->
-				<div class="collapse navbar-collapse" id="navbar-collapse">
+				<div class="collapse navbar-collapse" id="navbar-brand-centered">
 					<ul class="nav navbar-nav">
 						<li id="homeNav"><a href="index.php">Home</a></li>
+						<li id="galleryNav"><a href="gallery.php">Gallery</a></li>
 						<li id="menuNav"><a href="menu.php">Menu</a></li>
 						<li id="cateringNav"><a href="catering.php">Catering</a></li>
-						<li id="galleryNav"><a href="gallery.php">Gallery</a></li>
 					</ul>
-					<!--
-					<form class="navbar-form navbar-left">
-						<div class="form-group">
-							<input type="text" class="form-control" placeholder="Search">
-						</div>
-						<button type="submit" class="btn btn-default">Search</button>
-					</form>
-					-->
+					<ul class="nav navbar-nav navbar-right">
+						<li class="dropdown">
+							<a class="dropdown-toggle" data-toggle="dropdown" href="#">Connect With Us
+							<span class="caret"></span></a>
+							<ul class="dropdown-menu">
+								<a href="https://www.instagram.com/lisboabakery/"><img src="images/footer/insta.png" alt="Instagram Link" /></a>
+								<a href="https://www.facebook.com/lisboabakery/"><img src="images/footer/fb.png" alt="Facebook Link" /></a>
+								<a href="https://www.snapchat.com/add/lisboabakery"><img src="images/footer/sc.png" alt="Snapchat Link" /></a>
+								
+								
+							</ul>
+						</li>
+					</ul>
 				</div><!-- /.navbar-collapse -->
 			</div><!-- /.container-fluid -->
 		</nav>
